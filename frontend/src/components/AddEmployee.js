dc83fca8b286de4d57eaa4119729be6110338c7a
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-23 08:38:12 +1300

    chore: add input form

--- frontend/src/components/AddEmployee.js
+++ frontend/src/components/AddEmployee.js
@@ -7,6 +7,33 @@ const AddEmployee = () => {
         <div className="font-thin text-2xl tracking-wider">
           <h1>Add New Employee</h1>
         </div>
+        <div className="item-center justify-center h-14 w-full my-4">
+          <label className="block text-gray-600 text-sm font-normal">
+            First Name
+          </label>
+          <input
+            type="text"
+            className="h-10 w-96 border mt-2 px-2 py-2"
+          ></input>
+        </div>
+        <div className="item-center justify-center h-14 w-full my-4">
+          <label className="block text-gray-600 text-sm font-normal">
+            Last Name
+          </label>
+          <input
+            type="text"
+            className="h-10 w-96 border mt-2 px-2 py-2"
+          ></input>
+        </div>
+        <div className="item-center justify-center h-14 w-full my-4">
+          <label className="block text-gray-600 text-sm font-normal">
+            Email
+          </label>
+          <input
+            type="email"
+            className="h-10 w-96 border mt-2 px-2 py-2"
+          ></input>
+        </div>
       </div>
     </div>
   );
