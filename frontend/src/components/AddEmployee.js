8dd5f3d5426929bb195f9e3f4f6d7e8bc7d6515b
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-23 08:37:58 +1300

    chore: add AddEmployee component

---
+++ frontend/src/components/AddEmployee.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AddEmployee = () => {
+  return (
+    <div className="flex max-w-2xl shadow border-b mx-auto">
+      <div className="px-8 py-8">
+        <div className="font-thin text-2xl tracking-wider">
+          <h1>Add New Employee</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddEmployee;
