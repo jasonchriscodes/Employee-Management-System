55c76bd4c7ff30bff5690adab0b80b986a00ace1
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-23 14:17:49 +1300

    chore: add save and clear button

--- frontend/src/components/AddEmployee.js
+++ frontend/src/components/AddEmployee.js
@@ -34,6 +34,14 @@ const AddEmployee = () => {
             className="h-10 w-96 border mt-2 px-2 py-2"
           ></input>
         </div>
+        <div className="item-center justify-center h-14 w-full my-4 space-x-4 pt-4">
+          <button className="rounded text-white font-semibold bg-green-400 py-2 px-6 hover:bg-green-700">
+            Save
+          </button>
+          <button className="rounded text-white font-semibold bg-red-400 py-2 px-6 hover:bg-red-700">
+            Clear
+          </button>
+        </div>
       </div>
     </div>
   );
