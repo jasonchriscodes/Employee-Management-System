e05bf58609dc192a86826cd4c86eac72bb8bf5e6
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-24 01:22:18 +1300

    fix: cors policy

--- backend/employee-system-api/src/main/java/com/jason/employeesystemapi/controller/EmployeeController.java
+++ backend/employee-system-api/src/main/java/com/jason/employeesystemapi/controller/EmployeeController.java
@@ -3,11 +3,13 @@ package com.jason.employeesystemapi.controller;
 import com.jason.employeesystemapi.model.Employee;
 import com.jason.employeesystemapi.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
