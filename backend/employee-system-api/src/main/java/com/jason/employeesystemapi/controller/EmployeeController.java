ec68b39c8327083eaccc855b1939c27513f57d6e
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-23 15:55:59 +1300

    chore: add employeeService obj

--- backend/employee-system-api/src/main/java/com/jason/employeesystemapi/controller/EmployeeController.java
+++ backend/employee-system-api/src/main/java/com/jason/employeesystemapi/controller/EmployeeController.java
@@ -1,8 +1,19 @@
 package com.jason.employeesystemapi.controller;
 
+import com.jason.employeesystemapi.services.EmployeeService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/")
-public class EmployeeController {}
+public class EmployeeController {
+
+  @Autowired
+  private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+}
