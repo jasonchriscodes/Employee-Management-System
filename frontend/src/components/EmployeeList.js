d7f226f63435bc315327ecbf2c5ce752dc3b1288
mrb34n135421234 <jasonchristian1234@gmail.com>
2023-03-24 17:54:38 +1300

    chore: add AddEmployee button

--- frontend/src/components/EmployeeList.js
+++ frontend/src/components/EmployeeList.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const EmployeeList = () => {
   return (
-    <div>EmployeeList</div>
-  )
-}
+    <div className="container mx-auto my-8 ">
+      <div className="h-12">
+        <button className="rounded bg-slate-600 text-white px-6 py-2 font-semibold">
+          Add Employee
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default EmployeeList
+export default EmployeeList;
